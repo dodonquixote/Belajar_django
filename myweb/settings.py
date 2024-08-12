92ecaff89e3e25d29546ca361a671865ce7abdcb
dodonquixote <salmanalfarizita@gmail.com>
2024-08-12 21:48:09 +0700

    Updated Myweb function and urls

--- myweb/settings.py
+++ myweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,4 +140,16 @@ PROFILE_REDIRECT_URL = '/profile'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#auth login login.forms
+# massage
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
+
+#penyimpanan foto
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
