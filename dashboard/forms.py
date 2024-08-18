d7379aabbcdcd7fd1c4b9794e89c6de750658c4f
dodonquixote <salmanalfarizita@gmail.com>
2024-08-18 16:46:44 +0700

    Updated forms

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import BiodataMahasiswa
+from .models import BiodataMahasiswa, JadwalKuliah, MataKuliah, NilaiMahasiswa
 
 class BiodataMahasiswaForm(forms.ModelForm):
     class Meta:
@@ -11,4 +11,34 @@ class BiodataMahasiswaForm(forms.ModelForm):
             'tanggal_lahir': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'jenis_kelamin': forms.Select(attrs={'class': 'form-control'}),
             'foto' : forms.ClearableFileInput(attrs={'class' : 'form-control'})
+        }
+
+class JadwalKuliahForm(forms.ModelForm):
+    class Meta:
+        model = JadwalKuliah
+        fields = ['matkul', 'hari', 'jam']  # Hapus 'dosen' jika tidak ada di model
+        widgets = {
+            'hari': forms.Select(attrs={'class': 'form-control'}),  # Menggunakan Select untuk choices
+            'jam': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class MataKuliahForm(forms.ModelForm):
+    class Meta:
+        model = MataKuliah
+        fields = ['matkul', 'dosen', 'sks']
+        widgets = {
+            'matkul': forms.TextInput(attrs={'class': 'form-control'}),
+            'dosen': forms.TextInput(attrs={'class': 'form-control'}),
+            'sks': forms.Select(attrs={'class': 'form-control'}),  # Menggunakan Select untuk choices
+        }
+
+
+class NilaiMahasiswaForm(forms.ModelForm):
+    class Meta:
+        model = NilaiMahasiswa 
+        fields = ['matkul', 'nilai_huruf'] 
+        widgets = {
+            'matkul': forms.Select(attrs={'class': 'form-control'}),  # Menggunakan Select untuk ForeignKey
+            'nilai_huruf': forms.Select(attrs={'class': 'form-control'}),  # Menggunakan Select untuk choices
         }
